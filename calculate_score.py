44eb9244082f5b2f59a4ead8446cb4e2c64d702d
Chungsang Tom Lam <erinata@gmail.com>
2019-04-04 16:42:24 -0400

    split the data

--- calculate_score.py
+++ calculate_score.py
@@ -1,8 +1,9 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 dataset = pd.read_csv("dataset.csv")
 
-print(dataset.head())
+# print(dataset.head())
 
 data = dataset.iloc[:,3:10]
 
@@ -11,4 +12,26 @@ print(data.head())
 target = dataset.iloc[:,2].values
 
 print(target)
+
+data_training, data_test, target_training, target_test = train_test_split(data, target, test_size = 0.25, random_state = 0)
+
+print(data_training.head())
+print(data_test.head())
+print(target_training)
+print(target_test)
+
+print(data.shape)
+print(target.shape)
+print(data_training.shape)
+print(data_test.shape)
+print(target_training.shape)
+print(target_test.shape)
+
+
+
+
+
+
+
+
 
