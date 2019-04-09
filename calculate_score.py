4c57612d3f2b49852524e7fc65fd140af68f3d56
Chungsang Tom Lam <erinata@gmail.com>
2019-04-09 16:32:19 -0400

    use kfold

--- calculate_score.py
+++ calculate_score.py
@@ -3,6 +3,7 @@ from sklearn.model_selection import train_test_split
 from sklearn import linear_model
 from matplotlib import pyplot as plt
 from sklearn import metrics
+from sklearn.model_selection import KFold
 
 dataset = pd.read_csv("dataset.csv")
 
@@ -46,8 +47,26 @@ plt.savefig("scatter_test_prediction.png")
 print(metrics.r2_score(target_test,prediction))
 
 
+#===============================
 
 
+data = data.values
+
+print(data)
+
+kfold_machine = KFold(n_splits = 4)
+kfold_machine.get_n_splits(data)
+print(kfold_machine)
+
+for training_index, test_index in kfold_machine.split(data):
+	print("Training: ", training_index)
+	print("Test: ", test_index)
+	data_training, data_test = data[training_index], data[test_index]
+	target_training, target_test = target[training_index], target[test_index]
+	linear_machine = linear_model.LinearRegression()
+	linear_machine.fit(data_training,target_training)
+	prediction = linear_machine.predict(data_test)
+	print(metrics.r2_score(target_test,prediction))
 
 
 
